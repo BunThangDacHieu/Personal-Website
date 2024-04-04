ef814e871d67580ed8d1ef0c1bb5eca6c401cd4f
Bun <bunthang204@gmail.com>
2024-04-04 14:10:46 +0700

    Add New Post v1.2

--- Angular/src/app/admin/post/news-post/news-post.component.ts
+++ Angular/src/app/admin/post/news-post/news-post.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news-post',
   templateUrl: './news-post.component.html',
-  styleUrl: './news-post.component.css'
+  styleUrls: ['./news-post.component.css'] // Sửa styleUrl thành styleUrls
 })
-export class NewsPostComponent {
+export class NewsPostComponent implements OnInit {
+  permalink: string = '';
+  title: string = '';
+  imgSrc: string ='./assets/istockphoto-1147544807-612x612.jpg';
+  selectedImg: File | null = null; // Khai báo thuộc tính selectedImg
 
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  onTitleChange($event: any) {
+    this.title = $event.target.value;
+    this.generatePermalink();
+  }
+
+  generatePermalink() {
+    this.permalink = this.title.replace(/\s/g, '-');
+    console.log(this.permalink);
+  }
+
+  showPreview($event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result as string;
+    }
+    reader.readAsDataURL($event.target.files[0]);
+    this.selectedImg = $event.target.files[0];
+  }
 }
