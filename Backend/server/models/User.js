61785bf9db1b0c451d514743fd94845b8d03f71e
Bun <bunthang204@gmail.com>
2024-04-08 11:05:23 +0700

    Check error

--- Backend/server/models/User.js
+++ Backend/server/models/User.js
@@ -1,38 +1,49 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const crypto = require('crypto-js');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
-const UserSchema = mongoose.Schema(
-    {
-        "UserPassword": {
-            type: String,
-            unique: true,
-            required: true
-        },
-        "UserName": {
-            type: String,
-            unique: true,
-            match: [/^[a-zA-Z0-9]+$/, "is invalid"], // Kiểm tra ký tự hợp lệ
-            index: true,
-        },
-        "UserMail": {
-            type: String,
-            unique: true,
-            required: [true, "can't be blank"],
-            match: [/\S+@\S+\.\S+/, 'is invalid'], index: true
-        },
-        "IsAdmin": {
-            type: Boolean,
-            default: false,
-        }
+const UserSchema = new mongoose.Schema({
+    UserMail: {
+        type: String,
+        required: true,
+        unique: true
     },
-    {
-        timestamps: true, // Sửa "Timestamps" thành "timestamps"
-    }
-);
+    UserName: {
+        type: String,
+        unique: true,
+        match: /^[a-zA-Z0-9]+$/
+    },
+    UserPassword: {
+        type: String,
+        required: true,
+        select: false
+    },
+    IsAdmin: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true });
 
+UserSchema.plugin(uniqueValidator);
+
+UserSchema.methods.setPassword = async function (password) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    this.UserPassword = hashedPassword;
+};
+
+UserSchema.methods.validatePassword = async function (password) {
+    return await bcrypt.compare(password, this.UserPassword);
+};
+
+UserSchema.methods.generateJWT = function () {
+    return jwt.sign({
+        email: this.UserMail,
+        username: this.UserName,
+        isAdmin: this.IsAdmin
+    }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
